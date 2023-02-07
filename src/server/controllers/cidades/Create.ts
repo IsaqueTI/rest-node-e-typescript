daf74b3d8615fa6b4437a85fdc55a5eaf1e95ff3
isaque.cerqueira@live.com <isaque.cerqueira@live.com>
2023-02-07 06:32:01 -0300

    feat: added cidades model and updated cidade controller methods

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
+import { ICidade } from '../../database/models';
 import { validation } from '../../shared/middleware';
 
 
-interface ICidade {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, 'id'> { }
 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(yup.object().shape({
+  body: getSchema<IBodyProps>(yup.object().shape({
     nome: yup.string().required().min(3)
   }))
 }));
 
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
   console.log('teste', req.body);
   return res.status(StatusCodes.CREATED).json(1);
 };
